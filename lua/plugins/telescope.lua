3d38deb53276d80fd22e3cb7e52cf02809e17853
natty_dubs1 <96153503+nattydubs1@users.noreply.github.com>
2026-02-18 10:28:32 -0500

    Add telescope-fzf-native dependency and key mappings

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,11 +3,19 @@ return {
   "nvim-telescope/telescope.nvim",
   dependencies = {
     "nvim-lua/plenary.nvim",
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
   },
+  config = function()
+    require("telescope").setup({})
+    require("telescope").load_extension("fzf")
+  end,
   keys = {
     { "<leader>ff", "<cmd>Telescope find_files<cr>" },
     { "<leader>fg", "<cmd>Telescope live_grep<cr>" },
     { "<leader>fb", "<cmd>Telescope buffers<cr>" },
+    { "<leader>fh", "<cmd>Telescope help_tags<cr>" },
+    { "<leader>fr", "<cmd>Telescope oldfiles<cr>" },
+    { "<leader>fs", "<cmd>Telescope lsp_document_symbols<cr>" },
   },
 }
 
