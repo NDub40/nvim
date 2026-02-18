e9ae55f21e74aaa2a86e88c7973764522e0e8e88
natty_dubs1 <96153503+nattydubs1@users.noreply.github.com>
2026-02-18 10:35:59 -0500

    Add additional Vim options for improved editing

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,17 +4,21 @@ vim.opt.relativenumber = true
 vim.opt.mouse = "a"
 vim.opt.clipboard = "unnamedplus"
 vim.opt.termguicolors = true
-
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
 vim.opt.smartindent = true
-
 vim.opt.wrap = false
 vim.opt.scrolloff = 8
 vim.opt.sidescrolloff = 8
 vim.opt.signcolumn = "yes"
-
 vim.opt.updatetime = 300
 vim.opt.timeoutlen = 400
+vim.opt.cursorline = true
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.undofile = true
+vim.opt.completeopt = { "menu", "menuone", "noselect" }
 
